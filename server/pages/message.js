9404c626e534170c4510a4797378568b451b2c58
Krasimir <krasimir.st.tsonev@gmail.com>
2024-03-14 23:44:59 +0200

    Damn

--- server/pages/message.js
+++ server/pages/message.js
@@ -7,14 +7,32 @@ module.exports = async function (req, res) {
   try {
     browser = await createBrowser();
     await browser.open(GROUP);
+    await browser.page.click('[aria-label="Sign in"]');
+    await delay(300);
+    await browser.page.type('[type="email"]', USER);
+    await clickButtonWithLabel(browser, 'Next');
+    await browser.page.type('[type="password"]', PASSWORD);
+    await clickButtonWithLabel(browser, 'Next');
     await browser.snapshot();
     await browser.close();
   } catch (e) {
     console.error(e);
+    await browser.snapshot();
+    await browser.close();
     res.status(500);
     res.json({ error: e.message });
-    browser.close();
     return;
   }
   res.json({ ok: true });
+}
+
+async function clickButtonWithLabel(browser, label) {
+  let nextButton = await browser.page.$x('//button[contains(string(), "Next")]')
+  if (nextButton.length === 0) {
+    throw new Error(`Button with label ${label} not found`);
+  }
+  await nextButton[0].click();
+}
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
 }
