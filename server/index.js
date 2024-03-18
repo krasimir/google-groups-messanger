d4e374f40651702b63e554d104ee347f3552d130
Krasimir <krasimir.st.tsonev@gmail.com>
2024-03-18 23:17:56 +0200

    Adding basic auth

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const basicAuth = require('express-basic-auth');
+
+const { BASIC_AUTH_USER, BASIC_AUTH_PASSWORD } = require('../config.json');
+const message = require('./pages/message');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(bodyParser.json());
 
-const message = require('./pages/message');
-
-app.use(express.static(__dirname + '/public'));
-
+app.use('/', basicAuth({
+  challenge: true,
+  users: { [BASIC_AUTH_USER]: BASIC_AUTH_PASSWORD }
+}))
 app.post('/message', message);
+app.use('/', express.static(__dirname + '/public'));
 
 app.listen(PORT, () => {
   console.log(`Server running at ${PORT}`);
